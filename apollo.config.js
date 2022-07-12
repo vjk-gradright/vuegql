579751ef783179851f0b92e8365f572edf094aa5
vjk-gradright <vijay.b@gradright.com>
2022-07-12 16:20:01 +0530

    feat: installed vue apollo cli

---
+++ apollo.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  client: {
+    service: {
+      name: "my-app",
+      // URL to the GraphQL API
+      url: "http://localhost:5432/graphql",
+    },
+    // Files processed by the extension
+    includes: ["src/**/*.vue", "src/**/*.js"],
+  },
+};
